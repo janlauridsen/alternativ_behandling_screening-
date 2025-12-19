b0b405ad9855a68c29b8ca334bf0d098640b44b7
Jan Lauridsen <jan.lauridsen@hotmail.com>
2025-12-19 16:15:40 +0100

    Create README.mk

---
+++ docs/README.mk
@@ -0,0 +1,12 @@
+# Dokumentation
+
+Denne mappe indeholder den samlede dokumentation for projektet
+Alternativ Behandling Screening / Gaarsdal Assistent.
+
+Dokumenterne her beskriver:
+- projektets design og afgrænsning
+- tone og sproglige principper
+- juridiske overvejelser
+- beskrivelser af de metoder, systemet forholder sig til
+
+Dokumentationen er normativ og styrende for både indhold og implementering.
